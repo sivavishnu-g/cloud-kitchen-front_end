2e24e0d6a4187ddda58d29fab2b2e7fea81fd366
Sivavishnu <sivavishnu2424@gmail.com>
2025-04-01 23:19:41 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
-import "../styles/navbar.css"; 
 import { Link } from "react-router-dom";
+import "../styles/navbar.css"; 
 
 function Navbar() {
   return (
-    <div>
     <nav className="navbar">
-      <h1 className="logo">Cloud Kitchen</h1>
-      <p className="sub">Fresh & delicious meals delivered to your doorstep.</p>
-      <p></p>
+      <div className="navbar-left">
+        <h1 className="logo">Cloud Kitchen</h1>
+        <p className="sub">Fresh & delicious meals delivered to your doorstep.</p>
+      </div>
+      <Link to="/admin">
+        <button className="login-button">Admin Login</button>
+      </Link>
     </nav>
-         <Link to='/admin'> <button className="login-button">Admin Login</button> </Link>
-         </div>
   );
 }
 
